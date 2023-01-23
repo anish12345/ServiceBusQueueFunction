9209c3f812932468dd160c86cc66dd198eaeff28
Anish Zala <azala@affirma.com>
2023-01-23 10:19:06 +0530

    dfd

--- ServiceBusQueueFunction/Function1.cs
+++ ServiceBusQueueFunction/Function1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,10 +9,11 @@ namespace ServiceBusQueueFunction
 {
     public class Function1
     {
+     //   public void Run([ServiceBusTrigger("anishqueue", Connection = "connectionString")] string myQueueItem, ILogger log)
         [FunctionName("Function1")]
-        public void Run([ServiceBusTrigger("anishqueue", Connection = "connectionString")]string myQueueItem, ILogger log)
+        public void Run([ServiceBusTrigger("anishqueue", Connection = "connectionString")]Message queueMessage, ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {0} " ,Encoding.UTF8.GetString(queueMessage.Body));
         }
     }
 }
